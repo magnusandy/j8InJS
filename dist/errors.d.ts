46ea88b26a84dee97d80cc53d14244c7c04c51f0
Andrew Magnus <magnusandy@gmail.com>
2018-08-02 22:50:03 -0600

    removing a bunch of not needed folders

--- dist/errors.d.ts
+++
@@ -1,4 +0,0 @@
-export declare enum Errors {
-    NoSuchElementException = "NoSuchElementException",
-    NullPointerException = "NullPointerException"
-}
--- dist/errors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Errors;
-(function (Errors) {
-    Errors["NoSuchElementException"] = "NoSuchElementException";
-    Errors["NullPointerException"] = "NullPointerException";
-})(Errors = exports.Errors || (exports.Errors = {}));
