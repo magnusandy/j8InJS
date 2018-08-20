956cefce9653f65028138a31a4703e31f5217f43
Andrew Magnus <magnusandy@gmail.com>
2018-08-19 18:46:43 -0600

    finished function tests

--- test/functionTest.ts
+++ test/functionTest.ts
@@ -1,4 +1,4 @@
-import {Consumer, Transformer } from "../functions";
+import {Consumer, Transformer, BiPredicate, Comparator } from "../functions";
 import {use, spy, expect} from "chai";
 import * as spies from "chai-spies";
 use(spies);
@@ -35,6 +35,59 @@ describe('Function tests', () => {
             const returned = logger(valueStruct);
             expect(returned).to.equal(valueStruct);
             expect(consoleSpy).to.have.been.called.with(valueStruct);
+        });
+    });
+
+    describe('Comparator', () => {
+        it('default comparator returns -1 if first value less than second', () => {
+            const value1 = 1;
+            const value2 = 2;
+            const result = Comparator.default()(value1, value2);
+            expect(result).lessThan(0);
+        });
+
+        it('default comparator returns 1 if first value greater than than second', () => {
+            const value1 = 2;
+            const value2 = 1;
+            const result = Comparator.default()(value1, value2);
+            expect(result).greaterThan(0);
+        });
+
+        it('default comparator returns -1 if first value less than second', () => {
+            const value1 = 1;
+            const value2 = 1;
+            const result = Comparator.default()(value1, value2);
+            expect(result).to.equal(0);
+        });
+    });
+
+    describe('BiPredicate', () => {
+        it('defaultEquality returns a function that returns true if two items === each other', () => {
+            const value1 = 1;
+            const value2 = 1;
+            const result = BiPredicate.defaultEquality()(value1, value2);
+            expect(result).to.eq(true);
+        });
+
+        it('defaultEquality returns a function that returns false if two items do not ===', () => {
+            const value1 = 2;
+            const value2 = 1;
+            const result = BiPredicate.defaultEquality()(value1, value2);
+            expect(result).to.eq(false);
+        });
+
+        it('defaultEquality returns a function that returns true if two items === each other', () => {
+            const value1 = {i: '1'};
+            const value2 = {i: '1'};
+            const result = BiPredicate.defaultEquality()(value1, value2);
+            expect(result).to.eq(false);
+        });
+        
+        it('defaultEquality returns a function that returns true if two items === each other', () => {
+            const value1 = {i: '1'};
+            const value2 = value1;
+            const result = BiPredicate.defaultEquality()(value1, value2);
+            expect(result).to.eq(true);
         });
     });
 });
